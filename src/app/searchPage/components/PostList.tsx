61ad11f43896c4e43e677598d0fd3fc22acd19be
Bee0Theepob <theepob15112@gmail.com>
2025-02-10 22:53:19 +0700

    add responsive

--- src/app/searchPage/components/PostList.tsx
+++ src/app/searchPage/components/PostList.tsx
@@ -26,9 +26,9 @@ export const PostList = ({inputSearchValue}:{inputSearchValue:String}) => {
     <>
     <div className="flex flex-col item-center pt-8">
   <div className="flex flex-row gap-5 self-start">
-    <div className="ml-3.5 mr-auto">เรียงโดย</div>
-    <button onClick={()=>setPriceAsc(-1*priceAsc)} className="rounded-m bg-white text-sm">ราคา {priceAsc==1 ? "▲":"▼"}</button>
-    <button onClick={()=>setPopAsc(-1*popAsc)} className="rounded-m bg-white text-sm">ความนิยม {popAsc==1 ? "▲":"▼"}</button>
+    <div className="ml-3.5 mr-auto text-lg mt-1">เรียงโดย</div>
+    <button onClick={()=>setPriceAsc(-1*priceAsc)} className="rounded-lg border border-gray-300 bg-white text-sm text-lg p-2 ">ราคา <span className="ml-2">{priceAsc==1 ? "▲":"▼"}</span></button>
+    <button onClick={()=>setPopAsc(-1*popAsc)} className="rounded-lg border border-gray-300 bg-white text-sm text-lg p-2 ">ความนิยม <span className="ml-2">{popAsc==1 ? "▲":"▼"}</span></button>
   </div>
   <div className="flex flex-wrap w-full gap-5 m-2 p-2 pt-8 text-lg ml-1.5">
     {filter_posts.map((post: Post) => (
