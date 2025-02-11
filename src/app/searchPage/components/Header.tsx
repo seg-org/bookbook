95bdc54cd48f0a3f72e950e4dbca1bcbf0e7a4ad
Bee0Theepob <theepob15112@gmail.com>
2025-02-11 22:38:16 +0700

    updatePostPage

--- src/app/searchPage/components/Header.tsx
+++ src/app/searchPage/components/Header.tsx
@@ -1,27 +1,28 @@
 import Image from "next/image";
+import Link from "next/link";
 
 function Header() {
   return (
     <header className="flex flex-row items-center justify-between border-b border-gray-300 bg-white p-2.5">
-      <a href ="/" className="text-2xl font-bold leading-tight text-blue-500">
+      <Link href="/" className="text-2xl font-bold leading-tight text-blue-500">
         BOOK<br></br>BOOK
-      </a>
+      </Link>
       <nav>
         <ul className="flex list-none items-center justify-between gap-4">
-          <li className="sm:visible max-sm:invisible">
-            <a href="#" className="font-bold text-gray-800 hover:text-[#304674] hover:underline">
+          <li className="max-sm:invisible sm:visible">
+            <Link href="#" className="font-bold text-gray-800 hover:text-[#304674] hover:underline">
               ค้นหาหนังสือ
-            </a>
+            </Link>
           </li>
-          <li className="sm:visible max-sm:invisible">
-            <a href="#" className="font-bold text-gray-800 hover:text-[#304674] hover:underline">
+          <li className="max-sm:invisible sm:visible">
+            <Link href="#" className="font-bold text-gray-800 hover:text-[#304674] hover:underline">
               ประวัติการสั่งซื้อ
-            </a>
+            </Link>
           </li>
-          <li className="sm:visible max-sm:invisible">
-            <a href="#" className="font-bold text-gray-800 hover:text-[#304674] hover:underline">
+          <li className="max-sm:invisible sm:visible">
+            <Link href="#" className="font-bold text-gray-800 hover:text-[#304674] hover:underline">
               ติดต่อเรา
-            </a>
+            </Link>
           </li>
           <li>
             <div className="relative h-10 w-10 rounded-full bg-gray-300">
