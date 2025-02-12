552ecc24aac853092b08b0ffc4cbca5ce1aa4622
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2025-02-12 20:43:01 +0700

    fix: remove unused

--- src/app/transactionHistoryPage/components/filterBar.tsx
+++ src/app/transactionHistoryPage/components/filterBar.tsx
@@ -1,39 +1,36 @@
-import React from 'react'
-
-
 // Note 1: Waiting for global font
 // Note 2: The pop=up calendat can not be configured
 const FilterBar = () => {
   return (
-    <div className='flex lg:flex-row items-center p-2.5 justify-between flex-col'>
-      <div className='justify-start space-x-5 flex flex-row'>
-        <div className='space-x-2.5 items-center flex flex-row'>
-          <label className='text-gray-800 font-medium'>ตั้งแต่วันที่</label>
-          <input className='rounded-lg p-1 border border-gray-300' type='date'/>
-          <label className='text-gray-800 font-medium'>ถึงวันที่</label>
-          <input className='rounded-lg p-1 border border-gray-300' type='date'/>
+    <div className="flex flex-col items-center justify-between p-2.5 lg:flex-row">
+      <div className="flex flex-row justify-start space-x-5">
+        <div className="flex flex-row items-center space-x-2.5">
+          <label className="font-medium text-gray-800">ตั้งแต่วันที่</label>
+          <input className="rounded-lg border border-gray-300 p-1" type="date" />
+          <label className="font-medium text-gray-800">ถึงวันที่</label>
+          <input className="rounded-lg border border-gray-300 p-1" type="date" />
         </div>
-        <div className='space-x-2.5 items-center flex flex-row'>
-          <input className='h-5 w-5' type='checkbox'/>
-          <label className='text-gray-800 font-medium'>ซื้อ</label>
-          <input className='h-5 w-5' type='checkbox'/>
-          <label className='text-gray-800 font-medium'>ขาย</label>
+        <div className="flex flex-row items-center space-x-2.5">
+          <input className="h-5 w-5" type="checkbox" />
+          <label className="font-medium text-gray-800">ซื้อ</label>
+          <input className="h-5 w-5" type="checkbox" />
+          <label className="font-medium text-gray-800">ขาย</label>
         </div>
       </div>
-      <div className='justify-start space-x-10 flex flex-row items-center'>
-        <div className='space-x-2.5 items-center flex flex-row'>
-          <label className='text-gray-800 font-medium'>ยอดซื้อ :</label>
-          <label className='text-gray-800 font-medium'>30.0</label>
-          <label className='text-gray-800 font-medium'>ยอดขาย :</label>
-          <label className='text-gray-800 font-medium'>70.0</label>
+      <div className="flex flex-row items-center justify-start space-x-10">
+        <div className="flex flex-row items-center space-x-2.5">
+          <label className="font-medium text-gray-800">ยอดซื้อ :</label>
+          <label className="font-medium text-gray-800">30.0</label>
+          <label className="font-medium text-gray-800">ยอดขาย :</label>
+          <label className="font-medium text-gray-800">70.0</label>
         </div>
-        <div className='space-x-2.5 items-center flex flex-row'>
-          <label className='text-gray-800 font-medium'>ยอดรวม :</label>
-          <label className='text-gray-800 font-medium'>40.0</label>
+        <div className="flex flex-row items-center space-x-2.5">
+          <label className="font-medium text-gray-800">ยอดรวม :</label>
+          <label className="font-medium text-gray-800">40.0</label>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FilterBar
+export default FilterBar;
