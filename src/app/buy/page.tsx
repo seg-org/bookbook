fc6f81e5a06b21a23cd94423308302a81402ce31
Idhibhat Pankam <marginpankam@gmail.com>
2025-02-13 09:47:38 +0700

    fix: comment out code

--- src/app/buy/page.tsx
+++ src/app/buy/page.tsx
@@ -2,9 +2,7 @@
 import { Button } from "@/components/ui/Button";
 import { Card, CardContent } from "@/components/ui/card";
 import Dialog, { DialogContent, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/Dialog";
-import { Input } from "@/components/ui/Input";
 import { useRouter, useSearchParams } from "next/navigation";
-import path from "path";
 import { useState } from "react";
 
 export default function SellerInitiateTransaction() {
@@ -16,16 +14,24 @@ export default function SellerInitiateTransaction() {
   const encodedPostPriceString = searchParms.get("postPrice");
   const postPrice = encodedPostPriceString ? Number(decodeURIComponent(encodedPostPriceString)) : 0;
 
-  const [selectedBook, setSelectedBook] = useState(bookTitle);
-  const [buyerEmail, setBuyerEmail] = useState("");
-  const [negotiatedPrice, setNegotiatedPrice] = useState(postPrice);
+  // const [selectedBook, setSelectedBook] = useState(bookTitle);
+  const selectedBook = bookTitle;
+  // const [buyerEmail, setBuyerEmail] = useState("");
+  const buyerEmail = "  ";
+  // const [negotiatedPrice, setNegotiatedPrice] = useState(postPrice);
+  const negotiatedPrice = postPrice;
   const [openDialog, setOpenDialog] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [errors, setErrors] = useState<{
-    selectedBook?: string;
-    buyerEmail?: string;
-    negotiatedPrice?: string;
-  }>({});
+  // const [errors, setErrors] = useState<{
+  //   selectedBook?: string;
+  //   buyerEmail?: string;
+  //   negotiatedPrice?: string;
+  // }>({});
+  const errors = {
+    selectedBook: "",
+    buyerEmail: "",
+    negotiatedPrice: "",
+  };
   const router = useRouter();
 
   // Validate form before submission
@@ -46,7 +52,9 @@ export default function SellerInitiateTransaction() {
       const encodedSellerEmail = encodeURIComponent(buyerEmail);
       const encodedPostId = encodeURIComponent(postId);
       const encodedBookTitle = encodeURIComponent(selectedBook);
-      router.push(`/checkout?amount=${encodedNegotiatedPrice}&email=${encodedSellerEmail}&postId=${encodedPostId}&bookTitle=${encodedBookTitle}`);
+      router.push(
+        `/checkout?amount=${encodedNegotiatedPrice}&email=${encodedSellerEmail}&postId=${encodedPostId}&bookTitle=${encodedBookTitle}`
+      );
     } catch (error) {
       console.error("Error sending transaction invitation:", error);
       alert("Failed to send transaction invitation. Please try again.");
@@ -64,14 +72,14 @@ export default function SellerInitiateTransaction() {
           {/* Select Book (native dropdown) */}
           <div className="mt-4">
             <label className="mb-1 block font-medium">Book Title</label>
-            <p className="px-3 py-2 border rounded bg-gray-100">{bookTitle}</p>
+            <p className="rounded border bg-gray-100 px-3 py-2">{bookTitle}</p>
             {errors.selectedBook && <p className="text-sm text-red-500">{errors.selectedBook}</p>}
           </div>
 
           {/* Negotiated Price Input */}
           <div className="mt-4">
             <label className="mb-1 block font-medium">Price (THB)</label>
-            <p className="px-3 py-2 border rounded bg-gray-100">฿ {postPrice}</p>
+            <p className="rounded border bg-gray-100 px-3 py-2">฿ {postPrice}</p>
             {errors.negotiatedPrice && <p className="text-sm text-red-500">{errors.negotiatedPrice}</p>}
           </div>
 
--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -2,49 +2,49 @@
 
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
-import CheckoutPage from "./components/CheckoutPage";
 import { useSearchParams } from "next/navigation";
+import CheckoutPage from "./components/CheckoutPage";
 
 if (process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY === undefined) {
-    throw new Error("NEXT_PUBLIC_STRIPE_PUBLIC_KEY is not defined");
+  throw new Error("NEXT_PUBLIC_STRIPE_PUBLIC_KEY is not defined");
 }
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
 
 function Home() {
-    const searchParms = useSearchParams();
-    const encodedAmountString = searchParms.get("amount");
-    const encodedEmail = searchParms.get("email");
-    const amountString = encodedAmountString ? decodeURIComponent(encodedAmountString) : "";
-    const email = encodedEmail ? decodeURIComponent(encodedEmail) : "";
-    const encodedBookTitle = searchParms.get("bookTitle");
-    const bookTitle = encodedBookTitle ? decodeURIComponent(encodedBookTitle) : "";
-    const amount = amountString ? parseFloat(amountString) : 0;
-    const encodedPostId = searchParms.get("postId");
-    const postId = encodedPostId ? decodeURIComponent(encodedPostId) : "";
+  const searchParms = useSearchParams();
+  const encodedAmountString = searchParms.get("amount");
+  // const encodedEmail = searchParms.get("email");
+  const amountString = encodedAmountString ? decodeURIComponent(encodedAmountString) : "";
+  // const email = encodedEmail ? decodeURIComponent(encodedEmail) : "";
+  const encodedBookTitle = searchParms.get("bookTitle");
+  const bookTitle = encodedBookTitle ? decodeURIComponent(encodedBookTitle) : "";
+  const amount = amountString ? parseFloat(amountString) : 0;
+  const encodedPostId = searchParms.get("postId");
+  const postId = encodedPostId ? decodeURIComponent(encodedPostId) : "";
 
-    return (
-        <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-blue-500 to-purple-500">
-          <div className="mb-10">
-            <h1 className="text-4xl font-extrabold mb-2">{bookTitle}</h1>
-            <h2 className="text-2xl">
-              Price (THB): 
-              <span className="font-bold"> ฿{amount}</span>
-            </h2>
-          </div>
-    
-          <Elements
-            stripe={stripePromise}
-            options={{
-              mode: "payment",
-              amount: Math.round(amount * 100),
-              currency: "thb",
-              payment_method_types: ["card"],
-            }}
-          >
-            <CheckoutPage amount={amount} postId={postId} />
-          </Elements>
-        </main>
-      );
+  return (
+    <main className="m-10 mx-auto max-w-6xl rounded-md border bg-gradient-to-tr from-blue-500 to-purple-500 p-10 text-center text-white">
+      <div className="mb-10">
+        <h1 className="mb-2 text-4xl font-extrabold">{bookTitle}</h1>
+        <h2 className="text-2xl">
+          Price (THB):
+          <span className="font-bold"> ฿{amount}</span>
+        </h2>
+      </div>
+
+      <Elements
+        stripe={stripePromise}
+        options={{
+          mode: "payment",
+          amount: Math.round(amount * 100),
+          currency: "thb",
+          payment_method_types: ["card"],
+        }}
+      >
+        <CheckoutPage amount={amount} postId={postId} />
+      </Elements>
+    </main>
+  );
 }
 
 export default Home;
