9088defa5df8b594e89126a15113e3878544c367
Theepob <121664383+Bee0Theepob@users.noreply.github.com>
2025-02-14 14:40:35 +0700

    refactor: change var names

--- src/hooks/useGetRecommendPost.ts
+++ src/hooks/useGetRecommendPost.ts
@@ -3,21 +3,21 @@ import { getRecommendPosts } from "@/data/recommend-post";
 import { useEffect, useState } from "react";
 
 export const useGetRecommendPost = (userId: string) => {
-  const [recommend_post_loading, setRecommend_post_loading] = useState(true);
-  const [recommend_post_error, setRecommend_post_error] = useState<Error | null>(null);
-  const [recommend_post, setRecommend_post] = useState<Post>();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+  const [posts, setPosts] = useState<Post[]>([]);
 
   useEffect(() => {
     (async () => {
       const res = await getRecommendPosts(userId);
       if (res instanceof Error) {
-        return setRecommend_post_error(res);
+        return setError(res);
       }
 
-      setRecommend_post(res);
-      setRecommend_post_loading(false);
+      setPosts(res);
+      setLoading(false);
     })();
   }, [userId]);
 
-  return { recommend_post, recommend_post_loading, recommend_post_error };
+  return { posts, loading, error };
 };
