1ca8610d2ea1de262bac1856783c8a3e6e018fdd
markthitrin <thitrinwork@gmail.com>
2025-02-15 14:02:56 +0700

    feat : paginator front

--- src/app/transaction-history-page/components/PageBox.tsx
+++ src/app/transaction-history-page/components/PageBox.tsx
@@ -1,13 +1,37 @@
 import React from 'react'
 
 interface Props {
-    pageNumber: number
+    pageNumber: number,
+    selectingPage: number,
     setSelectingPage: (selectingPage: number) => void
 }
 
-const PageBox = ({ pageNumber, setSelectingPage } : Props) => {
+const PageBox = ({ pageNumber, setSelectingPage, selectingPage } : Props) => {
+  if(pageNumber == -1) {
+    return <div className="bg-white w-10 h-10 flex items-center justify-center text-black border border-gray-300 rounded-lg shadow transform hover:scale-110 transition-all duration-300"
+      onClick={() => setSelectingPage(selectingPage - 1)}>
+      {"<"}
+    </div>
+  }
+  if(pageNumber == -2) {
+    return <div className="bg-gray-400 w-10 h-10 flex items-center justify-center text-black border border-gray-300 rounded-lg shadow ">
+      {"<"}
+    </div>
+  }
+  if(pageNumber == -3) {
+    return <div className="bg-white w-10 h-10 flex items-center justify-center text-black border border-gray-300 rounded-lg shadow transform hover:scale-110 transition-all duration-300"
+      onClick={() => setSelectingPage(selectingPage + 1)}>
+      {">"}
+    </div>
+  }
+  if(pageNumber == -4) {
+    return <div className="bg-gray-400 w-10 h-10 flex items-center justify-center text-black border border-gray-300 rounded-lg shadow ">
+      {">"}
+    </div>
+  }
+
   return (
-    <div className="w-10 h-10 flex items-center justify-center bg-white text-black border border-gray-300 rounded-full shadow"
+    <div className={`${pageNumber === selectingPage ? 'bg-blue-500' : 'bg-white'} w-10 h-10 flex items-center justify-center text-black border border-gray-300 rounded-lg shadow  {}transform hover:scale-110 transition-all duration-300"`}
       onClick={() => setSelectingPage(pageNumber)}>
       {pageNumber}
     </div>
--- src/app/transaction-history-page/components/Paginator.tsx
+++ src/app/transaction-history-page/components/Paginator.tsx
@@ -1,6 +1,5 @@
 "use client"
-import React, { useEffect, useMemo } from 'react'
-import { number } from 'zod'
+import React, { useEffect, useMemo, useState } from 'react'
 import PageBox from "./PageBox"
 
 interface Props {
@@ -10,12 +9,25 @@ interface Props {
 }
 
 const Paginator = ({maxPageNumber, selectingPage, setSelectingPage} : Props) => {
+  const [ childComponents, setChildCompoent ] = useState<React.JSX.Element[]>([])
 
-  const child_components: React.JSX.Element[] = []
+  useEffect(() =>{
+    const newChildComponents: React.JSX.Element[] = []
+    const leftPageNumber = Math.max(Math.min(selectingPage - 2, maxPageNumber - 4), 1)
+    const rightPageNumber = Math.min(Math.max(selectingPage + 2, 5),maxPageNumber)
+    
+    newChildComponents.push(<PageBox key={-1} pageNumber={selectingPage != 1 ? -1 : -2} selectingPage={selectingPage} setSelectingPage={setSelectingPage} />)
+    for(let i = leftPageNumber;i <= rightPageNumber;i++) {
+      newChildComponents.push(<PageBox key={i} pageNumber={i} selectingPage={selectingPage} setSelectingPage={setSelectingPage} />)
+    }
+    newChildComponents.push(<PageBox key={-2} pageNumber={rightPageNumber != selectingPage ? -3 : -4} selectingPage={selectingPage} setSelectingPage={setSelectingPage} />)
+    
+    setChildCompoent(newChildComponents)
+  }, [maxPageNumber, selectingPage])
 
   return (
-    <div>
-      {child_components}
+    <div className="flex justify-center items-center gap-2 w-full mb-4">
+      {childComponents}
     </div>
   )
 }
