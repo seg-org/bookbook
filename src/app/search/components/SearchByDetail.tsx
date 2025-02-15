c5e2d90b8174a321506a7a10ac17de1b1d5a2090
Theepob <121664383+Bee0Theepob@users.noreply.github.com>
2025-02-15 21:28:35 +0700

    change var name

--- src/app/search/components/SearchByDetail.tsx
+++ src/app/search/components/SearchByDetail.tsx
@@ -3,10 +3,13 @@ function SearchByDetail() {
     <>
       <div className="mb-3 pr-3 font-semibold">ค้นหาหนังสือด้วยรายละเอียดเพิ่มเติม</div>
       <form className="grid gap-2.5">
-        <input className="rounded-sm border border-gray-300 p-2.5" type="text" placeholder="ชื่อหนังสือ"></input>
-        <input className="rounded-sm border border-gray-300 p-2.5" type="text" placeholder="ผู้เขียน"></input>
-        <input className="rounded-sm border border-gray-300 p-2.5" type="text" placeholder="สำนักพิมพ์"></input>
-        <input className="rounded-sm border border-gray-300 p-2.5" type="text" placeholder="ISBN"></input>
+        <input className="rounded-md border border-gray-300 p-2.5" type="text" placeholder="ชื่อหนังสือ"></input>
+        <input className="rounded-md border border-gray-300 p-2.5" type="text" placeholder="ผู้เขียน"></input>
+        <input className="rounded-md border border-gray-300 p-2.5" type="text" placeholder="สำนักพิมพ์"></input>
+        <input className="rounded-md border border-gray-300 p-2.5" type="text" placeholder="ISBN"></input>
+        <button className="cursor-pointer rounded-md border-none bg-[#9dc4de] p-2.5 text-white">
+          ค้นหาข้อมูลด้วยรายละเอียดเพิ่มเติม
+        </button>
       </form>
     </>
   );
