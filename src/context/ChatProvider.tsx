9ab331d92b35d988101238f9143bda5c398deb1f
Idhibhat Pankam <marginpankam@gmail.com>
2025-02-16 17:09:02 +0700

    use immer

--- src/context/ChatProvider.tsx
+++ src/context/ChatProvider.tsx
@@ -5,6 +5,7 @@ import { FC, PropsWithChildren, useState } from "react";
 import { readMessages } from "@/data/chat";
 import { ChatMessage, ChatRoom } from "@/data/dto/chat.dto";
 import { useGetMyChatRooms } from "@/hooks/useGetMyChatRooms";
+import { produce } from "immer";
 import { ChatContext } from "./chatContext";
 
 export const ChatProvider: FC<PropsWithChildren> = ({ children }) => {
@@ -13,31 +14,30 @@ export const ChatProvider: FC<PropsWithChildren> = ({ children }) => {
   const [messages, setMessages] = useState<ChatMessage[]>([]);
 
   const changeCurrentRoom = async (chatRoom: ChatRoom, userId: string) => {
-    setChatRooms((prev) => {
-      const idx = prev.findIndex((cr) => cr.id === chatRoom.id);
-      const updated = [...prev];
-      if (idx !== -1) {
-        const lastRead = userId === chatRoom.userIds[0] ? "lastReadA" : "lastReadB";
-        updated[idx] = {
-          ...updated[idx],
-          [lastRead]: new Date(),
-        };
-      }
-      return updated;
-    });
+    setChatRooms(
+      produce((draft) => {
+        const idx = draft.findIndex((cr) => cr.id === chatRoom.id);
+        if (idx !== -1) {
+          const lastRead = userId === chatRoom.userIds[0] ? "lastReadA" : "lastReadB";
+          draft[idx] = {
+            ...draft[idx],
+            [lastRead]: new Date(),
+          };
+        }
+      })
+    );
 
     setCurrentChatRoom(chatRoom);
     await readMessages(chatRoom.id, userId);
   };
 
   const updateChatRoomLastMessage = (messageId: string, roomId: string, message: string, senderId: string) => {
-    setChatRooms((prev) => {
-      const idx = prev.findIndex((cr) => cr.id === roomId);
-      const updated = [...prev];
-      const lastRead = senderId === updated[idx].userIds[0] ? "lastReadA" : "lastReadB";
-      if (idx !== -1) {
-        updated[idx] = {
-          ...updated[idx],
+    setChatRooms(
+      produce((draft) => {
+        const idx = draft.findIndex((cr) => cr.id === roomId);
+        const lastRead = senderId === draft[idx].userIds[0] ? "lastReadA" : "lastReadB";
+        draft[idx] = {
+          ...draft[idx],
           lastMessage: {
             id: messageId,
             senderId,
@@ -47,9 +47,8 @@ export const ChatProvider: FC<PropsWithChildren> = ({ children }) => {
           },
           [lastRead]: new Date(),
         };
-      }
-      return updated;
-    });
+      })
+    );
   };
 
   return (
