76241262480485021564e1729036a67665a37dc5
Theepob <121664383+Bee0Theepob@users.noreply.github.com>
2025-02-16 18:41:32 +0700

    API part finished

---
+++ src/app/api/posts/filtered/route.ts
@@ -0,0 +1,48 @@
+import { prisma } from "@/lib/prisma";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { NextRequest, NextResponse } from "next/server";
+import { getPresignedUrl } from "../../objects/s3";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export async function POST(req: Request) {
+  const {title, author, publisher, isbn} = await req.json();
+  console.log(title);
+  try {
+    const posts1 = await prisma.post.findMany({
+      include: { book: true },
+      where : {
+        AND : [
+            // {book: {path:["title"], contains: title, mode: "insensitive" }} ,
+            // {book: {path:["author"], contains: author, mode: "insensitive" }}, 
+            // {book: {path:["publisher"], contains: publisher, mode: "insensitive" }},
+            // {book: {path:["isbn"], contains: isbn, mode: "insensitive" }},
+        ]
+      }
+    });
+
+    const posts = posts1.filter(post => {
+        return  post.book.title.toLowerCase().includes(title.toLowerCase()) &&
+                post.book.author.toLowerCase().includes(author.toLowerCase()) &&
+                post.book.isbn.toLowerCase().includes(isbn.toLowerCase());
+    });
+    
+
+    const postsWithImageUrl = await Promise.all(
+      posts.map(async (post) => {
+        const url = await getPresignedUrl("book_images", post.book.coverImageKey);
+        return {
+          ...post,
+          book: {
+            ...post.book,
+            coverImageUrl: url,
+          },
+        };
+      })
+    );
+
+    return NextResponse.json(postsWithImageUrl);
+  } catch (error) {
+    if (error instanceof Error) console.error("Error getting posts", error.stack);
+    return NextResponse.json({ error: "Cannot get posts" }, { status: 500 });
+  }
+}
--- src/app/search/components/SearchByDetail.tsx
+++ src/app/search/components/SearchByDetail.tsx
@@ -1,12 +1,63 @@
+import React, { useState } from "react";
+
 function SearchByDetail() {
+  const [formData, setFormData] = useState({ title: "", author: "", publisher: "", isbn: "" });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    // console.log("Form Data:", formData);
+    const response = await fetch("/api/auth/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      // body:
+    });
+
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(data.error || "การลงทะเบียนล้มเหลว");
+    }
+  };
+
   return (
     <>
       <div className="mb-3 pr-3 font-semibold">ค้นหาหนังสือด้วยรายละเอียดเพิ่มเติม</div>
-      <form className="grid gap-2.5">
-        <input className="rounded-md border border-gray-300 p-2.5" type="text" placeholder="ชื่อหนังสือ"></input>
-        <input className="rounded-md border border-gray-300 p-2.5" type="text" placeholder="ผู้เขียน"></input>
-        <input className="rounded-md border border-gray-300 p-2.5" type="text" placeholder="สำนักพิมพ์"></input>
-        <input className="rounded-md border border-gray-300 p-2.5" type="text" placeholder="ISBN"></input>
+      <form className="grid gap-2.5" onSubmit={handleSubmit}>
+        <input
+          className="rounded-md border border-gray-300 p-2.5"
+          name="title"
+          type="text"
+          placeholder="ชื่อหนังสือ"
+          value={formData.title}
+          onChange={handleChange}
+        ></input>
+        <input
+          className="rounded-md border border-gray-300 p-2.5"
+          name="author"
+          type="text"
+          placeholder="ผู้เขียน"
+          value={formData.author}
+          onChange={handleChange}
+        ></input>
+        <input
+          className="rounded-md border border-gray-300 p-2.5"
+          name="publisher"
+          type="text"
+          placeholder="สำนักพิมพ์"
+          value={formData.publisher}
+          onChange={handleChange}
+        ></input>
+        <input
+          className="rounded-md border border-gray-300 p-2.5"
+          name="isbn"
+          type="text"
+          placeholder="ISBN"
+          value={formData.isbn}
+          onChange={handleChange}
+        ></input>
         <button className="cursor-pointer rounded-md border-none bg-[#9dc4de] p-2.5 text-white">
           ค้นหาข้อมูลด้วยรายละเอียดเพิ่มเติม
         </button>
