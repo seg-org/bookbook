140091541d85474a42fbdf43a830792227c03f35
markthitrin <95044715+markthitrin@users.noreply.github.com>
2025-02-22 12:44:48 +0700

    Us4 7/transaction history fix (#37)
    * fix : transaction fail seed's detail
    
    * fix : transaction box text overflow
    
    * fix : useEffect reder
    
    * feat : paginator front
    
    * feat : connect to user login
    
    * feat : working paginator
    
    * feat : Transaction detail oppup window
    fix : error and loading state never change in useGetTransaction
    
    * feat : complete popup detail transaction
    
    * fix : remove unused variable
    
    * fix : add more transaction fail type, fix transaction status, fix API
    
    * fix : add shipment method
    
    * fix : useGetTransaction systax
    feat : transactionContext
    
    * fix : useContext
    
    * fix : code refactor

--- src/app/api/transaction/route.ts
+++ src/app/api/transaction/route.ts
@@ -1,5 +1,5 @@
 import { prisma } from "@/lib/prisma";
-import { TransactionStatus } from "@prisma/client";
+import { PaymentMethod, ShipmentMethod, TransactionStatus } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 import { getUrl } from "../objects/s3";
@@ -8,38 +8,40 @@ const createTransactionRequest = z.object({
   buyerId: z.string(),
   postId: z.string(),
 
-  paymentMethod: z.enum(["CREDIT_CARD", "ONLINE_BANKING"]),
-  hashId: z.string(),
-
-  shipmentMethod: z.enum(["DELIVERY"]),
-  trackingURL: z.string(),
-
-  amount: z.number(),
+  amount: z.number().nonnegative(),
 });
 
 const beginningOfTime = new Date("0000-01-01T00:00:00Z");
 const endOfTime = new Date("9999-12-31T23:59:59Z");
 
+const parseToBoolean = (dft: boolean) => {
+  return (val: string | undefined) => (val ? val == "true" : dft);
+};
+
+const parseToPosInt = (dft: number) => {
+  return (val: string | undefined) => (val ? Math.max(parseInt(val), 0) : dft);
+};
+
+const parseToDate = (dft: Date) => {
+  return (val: string | undefined) => (val ? new Date(val) : dft);
+};
+
 const getTransactionRequest = z.object({
-  userId: z.string().default(() => ""),
+  userId: z.string().optional(),
   startDate: z
     .string()
     .optional()
     .refine((val) => !val || !isNaN(Date.parse(val)), { message: "Invalid date format" })
-    .transform((val) => (val == undefined ? beginningOfTime : new Date(val))),
+    .transform(parseToDate(beginningOfTime)),
   endDate: z
     .string()
     .optional()
     .refine((val) => !val || !isNaN(Date.parse(val)), { message: "Invalid date format" })
-    .transform((val) => (val == undefined ? endOfTime : new Date(val))),
-  asBuyer: z
-    .string()
-    .optional()
-    .transform((val) => (val == undefined ? true : val == "true")),
-  asSeller: z
-    .string()
-    .optional()
-    .transform((val) => (val == undefined ? true : val == "true")),
+    .transform(parseToDate(endOfTime)),
+  asBuyer: z.string().optional().transform(parseToBoolean(true)),
+  asSeller: z.string().optional().transform(parseToBoolean(true)),
+  skip: z.string().optional().transform(parseToPosInt(0)),
+  take: z.string().optional().transform(parseToPosInt(-1)),
 });
 
 export async function POST(req: NextRequest) {
@@ -49,15 +51,19 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ error: parsedData.error.errors }, { status: 400 });
     }
 
-    const buyer = await prisma.user.findUnique({
+    const buyer = await prisma.user.count({
       where: { id: parsedData.data.buyerId },
     });
-    if (!buyer) {
+    if (buyer !== 1) {
       return NextResponse.json({ error: `buyer with id ${parsedData.data.buyerId} not found` }, { status: 404 });
     }
 
     const post = await prisma.post.findUnique({
       where: { id: parsedData.data.postId },
+      select: {
+        published: true,
+        sellerId: true,
+      },
     });
     if (!post) {
       return NextResponse.json({ error: `post with id ${parsedData.data.postId} not found` }, { status: 404 });
@@ -70,8 +76,12 @@ export async function POST(req: NextRequest) {
       data: {
         ...parsedData.data,
         sellerId: post.sellerId,
-        status: TransactionStatus.PAYING,
+        status: TransactionStatus.APPROVING,
+        paymentMethod: PaymentMethod.UNDEFINED,
+        hashId: "",
         amount: parsedData.data.amount,
+        shipmentMethod: ShipmentMethod.UNDEFINED,
+        trackingURL: "",
         isDelivered: false,
       },
     });
@@ -93,12 +103,14 @@ export async function GET(req: NextRequest) {
     }
 
     const transactions = await prisma.transaction.findMany({
+      skip: parsedData.data.skip,
+      ...(parsedData.data.take !== -1 ? { take: parsedData.data.take } : {}),
       where: {
         createdAt: {
           gte: parsedData.data.startDate,
           lte: parsedData.data.endDate,
         },
-        ...(parsedData.data.userId !== ""
+        ...(parsedData.data.userId
           ? {
               OR: [
                 parsedData.data.asBuyer ? { buyerId: parsedData.data.userId } : {},
--- src/app/transaction-history-page/components/TransactionBox.tsx
+++ src/app/transaction-history-page/components/TransactionBox.tsx
@@ -1,38 +1,48 @@
 "use client";
 
-import { Transaction } from "@/data/dto/transaction.dto";
-import { TransactionStatus } from "@prisma/client";
 import Image from "next/image";
 
+import { useTransactionContext } from "@/context/transactionContext";
+import { Transaction } from "@/data/dto/transaction.dto";
+import { TransactionStatus } from "@prisma/client";
 import BoughtIcon from "../pic/boughtIcon.png";
 import SoldIcon from "../pic/soldIcon.png";
 
-// Note 1: type and sttatus should be enum type
-// Note 2: currently using image dummy source
+const cap_overflow_string = (str: string) => {
+  if (str.length >= 30) return str.substring(0, 30) + "...";
+  else return str;
+};
 
-export interface TransactionBoxProps {
-  transaction: Transaction;
-  type: string;
-}
+const TransactionBox = ({ transaction }: { transaction: Transaction }) => {
+  const { userId, setSelectingTransaction } = useTransactionContext();
 
-const TransactionBox = ({ transaction, type }: TransactionBoxProps) => {
-  const cap_overflow_string = (str: string) => {
-    if (str.length >= 30) return str.substring(0, 30) + "...";
-    else return str;
+  const statusMap: Record<TransactionStatus, { label: string; color: string }> = {
+    [TransactionStatus.APPROVING]: { label: "Approving", color: "text-gray-300" },
+    [TransactionStatus.PAYING]: { label: "Paying", color: "text-gray-300" },
+    [TransactionStatus.PACKING]: { label: "Packing", color: "text-gray-300" },
+    [TransactionStatus.DELIVERING]: { label: "Delivering", color: "text-gray-300" },
+    [TransactionStatus.COMPLETE]: { label: "Complete", color: "text-green-500" },
+    [TransactionStatus.HOLD]: { label: "Hold", color: "text-yellow-500" },
+    [TransactionStatus.FAIL]: { label: "Failed", color: "text-red-500" },
   };
 
   return (
-    <div className="relative flex transform flex-row items-center justify-between gap-x-10 rounded-lg border border-gray-300 bg-white p-4 transition-transform duration-200 hover:scale-105 hover:shadow-xl">
+    <div
+      className="relative flex transform flex-row items-center justify-between gap-x-5 rounded-lg border border-gray-300 bg-white p-4 transition-transform duration-200 hover:scale-105 hover:shadow-xl"
+      onClick={() => {
+        setSelectingTransaction(transaction.id);
+      }}
+    >
       <div className="flex flex-row items-center justify-start gap-5">
         <Image
           className="m-2.5 h-40 w-auto rounded-lg"
           src={transaction.post.book.coverImageUrl}
           alt="Book Cover"
-          height={90}
-          width={160}
+          height={160}
+          width={90}
         ></Image>
         <div className="flex flex-col">
-          <label className="text-xl font-semibold">{cap_overflow_string(transaction.post.book.title)}</label>
+          <label className="text-lg font-semibold">{cap_overflow_string(transaction.post.book.title)}</label>
           <label className="text-lg text-gray-400">
             {transaction.createdAt.toDateString()} {transaction.createdAt.getHours()}:
             {transaction.createdAt.getMinutes()}
@@ -42,16 +52,14 @@ const TransactionBox = ({ transaction, type }: TransactionBoxProps) => {
       </div>
 
       <div className="flex flex-col items-center justify-end text-xl font-bold">
-        {(transaction.status == TransactionStatus.APPROVING && <label className="text-gray-300">Approving</label>) ||
-          (transaction.status == TransactionStatus.PAYING && <label className="text-gray-300">Paying</label>) ||
-          (transaction.status == TransactionStatus.VERIFYING && <label className="text-gray-300">Verifying</label>) ||
-          (transaction.status == TransactionStatus.COMPLETE && <label className="text-green-500">Complete</label>) ||
-          (transaction.status == TransactionStatus.FAIL && <label className="text-red-500">Failed</label>)}
+        <label className={`text-lg ${statusMap[transaction.status]?.color}`}>
+          {statusMap[transaction.status]?.label}
+        </label>
       </div>
 
       <div className="absolute bottom-2 right-2">
-        {(type == "buy" && <Image src={BoughtIcon} alt="" width={25} height={25}></Image>) ||
-          (type == "sell" && <Image src={SoldIcon} alt="" width={25} height={25}></Image>)}
+        {(userId == transaction.buyerId && <Image src={BoughtIcon} alt="" width={25} height={25}></Image>) ||
+          (userId == transaction.sellerId && <Image src={SoldIcon} alt="" width={25} height={25}></Image>)}
       </div>
     </div>
   );
