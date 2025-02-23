86a119f238d54a4619de3d0545fdf794fed882ae
Nutthapat Pongtanyavichai <contact@leomotors.me>
2025-02-23 23:32:26 +0700

    fix prepare script

--- prepare-e2e.bash
+++ prepare-e2e.bash
@@ -7,9 +7,9 @@ docker compose -f docker-compose.e2e.yaml up -d
 # Wait 5 Seconds
 sleep 5
 
-docker exec -it e2e_s3 mc alias set e2e http://localhost:9000 admin password
-docker exec -it e2e_s3 mc mb e2e/bookbook
-docker exec -it e2e_s3 mc anonymous set public e2e/bookbook
+docker exec e2e_s3 mc alias set e2e http://localhost:9000 admin password
+docker exec e2e_s3 mc mb e2e/bookbook
+docker exec e2e_s3 mc anonymous set public e2e/bookbook
 
 pnpm prisma migrate deploy
 pnpm prisma db seed
