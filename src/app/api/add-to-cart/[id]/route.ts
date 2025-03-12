758ae3c41498dfe49da289c4d903099f4ac98e32
Nutthapat Pongtanyavichai <contact@leomotors.me>
2025-03-12 22:47:38 +0700

    format

--- src/app/api/add-to-cart/[id]/route.ts
+++ src/app/api/add-to-cart/[id]/route.ts
@@ -1,5 +1,6 @@
-import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
+
+import { prisma } from "@/lib/prisma";
 
 export async function GET(req: NextRequest, props: { params: Promise<{ id: string }> }) {
   const { id } = await props.params;
