b9c4a0002e186bac8f7a9d7862559133cce44956
Chayanin Kongsareekul <57552210+Aphrodicez@users.noreply.github.com>
2025-03-12 22:25:59 +0700

    fix: merge conflict

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,4 +1,7 @@
-import * as React from "react"
+"use client";
+
+import clsx from "clsx";
+import React from "react";
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
