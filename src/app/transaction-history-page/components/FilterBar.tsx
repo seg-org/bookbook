a0dd8c6fc07e101270f3ab765d43b7f8fba3024b
markthitrin <thitrinwork@gmail.com>
2025-03-22 17:31:49 +0700

    fix : front-end filter

--- src/app/transaction-history-page/components/FilterBar.tsx
+++ src/app/transaction-history-page/components/FilterBar.tsx
@@ -1,4 +1,8 @@
 import { useTransactionContext } from "@/context/transactionContext";
+import Image from "next/image";
+
+import BoughtIcon from "../pic/boughtIcon.png";
+import SoldIcon from "../pic/soldIcon.png";
 
 const beginningOfTime = new Date("0000-01-01T00:00:00Z");
 const endOfTime = new Date("9999-12-31T23:59:59Z");
@@ -12,13 +16,13 @@ const FilterBar = () => {
         <div className="flex flex-row items-center space-x-2.5">
           <label className="font-medium text-white">ตั้งแต่วันที่</label>
           <input
-            className="transform rounded-lg border border-gray-300 p-1 transition-transform duration-200 hover:scale-105 hover:shadow-xl"
+            className="transform rounded-lg border border-gray-300 p-1 text-blue-950 transition-transform duration-200 hover:scale-105 hover:shadow-xl"
             type="date"
             onChange={(e) => filter.setStartDate(e.target.value ? new Date(e.target.value) : beginningOfTime)}
           />
           <label className="font-medium text-white">ถึงวันที่</label>
           <input
-            className="transform rounded-lg border border-gray-300 p-1 transition-transform duration-200 hover:scale-105 hover:shadow-xl"
+            className="transform rounded-lg border border-gray-300 p-1 text-blue-950 transition-transform duration-200 hover:scale-105 hover:shadow-xl"
             type="date"
             onChange={(e) => filter.setEndDate(e.target.value ? new Date(e.target.value) : endOfTime)}
           />
@@ -26,13 +30,13 @@ const FilterBar = () => {
         <div className="h-auto border-l-2 border-blue-300"></div>
         <div className="flex flex-row items-center space-x-2.5">
           <button
-            className={`h-10 w-20 rounded-lg ${filter.asBuyer ? "bg-indigo-500 text-white" : "bg-blue-200 text-gray-600"} transition-transform duration-200 hover:scale-105 hover:shadow-lg`}
+            className={`h-10 w-20 rounded-lg ${filter.asBuyer ? "bg-indigo-500 text-white" : "bg-blue-200 text-sky-950"} transition-transform duration-200 hover:scale-105 hover:shadow-lg`}
             onClick={() => filter.setAsBuyer(!filter.asBuyer)}
           >
             ซื้อ
           </button>
           <button
-            className={`h-10 w-20 rounded-lg ${filter.asSeller ? "bg-indigo-500 text-white" : "bg-blue-200 text-gray-600"} transition-transform duration-200 hover:scale-105 hover:shadow-lg`}
+            className={`h-10 w-20 rounded-lg ${filter.asSeller ? "bg-indigo-500 text-white" : "bg-blue-200 text-sky-950"} transition-transform duration-200 hover:scale-105 hover:shadow-lg`}
             onClick={() => filter.setAsSeller(!filter.asSeller)}
           >
             ขาย
@@ -41,23 +45,40 @@ const FilterBar = () => {
         <div className="h-auto border-l-2 border-blue-300"></div>
         <div className="flex flex-row items-center space-x-2.5">
           <button
-            className={`h-10 w-20 rounded-lg ${filter.asSeller ? "bg-indigo-500 text-white" : "bg-blue-200 text-gray-600"} transition-transform duration-200 hover:scale-105 hover:shadow-lg`}
-            onClick={() => filter.setAsSeller(!filter.asSeller)}
+            className={`h-10 w-20 rounded-lg ${filter.isDelivering ? "bg-indigo-500 text-white" : "bg-blue-200 text-sky-950"} transition-transform duration-200 hover:scale-105 hover:shadow-lg`}
+            onClick={() => filter.setIsDelivering(!filter.isDelivering)}
           >
-            ขาย
+            ดำเนินการ
+          </button>
+          <button
+            className={`h-10 w-20 rounded-lg ${filter.isHold ? "bg-indigo-500 text-white" : "bg-blue-200 text-sky-950"} transition-transform duration-200 hover:scale-105 hover:shadow-lg`}
+            onClick={() => filter.setIsHold(!filter.isHold)}
+          >
+            ตรวจสอบ
+          </button>
+          <button
+            className={`h-10 w-20 rounded-lg ${filter.isCompleted ? "bg-indigo-500 text-white" : "bg-blue-200 text-sky-950"} transition-transform duration-200 hover:scale-105 hover:shadow-lg`}
+            onClick={() => filter.setIsCompleted(!filter.isCompleted)}
+          >
+            สำเร็จ
+          </button>
+          <button
+            className={`h-10 w-20 rounded-lg ${filter.isFailed ? "bg-indigo-500 text-white" : "bg-blue-200 text-sky-950"} transition-transform duration-200 hover:scale-105 hover:shadow-lg`}
+            onClick={() => filter.setIsFailed(!filter.isFailed)}
+          >
+            ยกเลิก
           </button>
         </div>
       </div>
-      <div className="flex flex-row items-center justify-start space-x-10">
-        <div className="flex flex-row items-center space-x-2.5">
-          <label className="font-medium text-white">ยอดซื้อ :</label>
-          <label className="font-medium text-white">{totalBuy.toFixed(2).toString()}</label>
-          <label className="font-medium text-white">ยอดขาย :</label>
-          <label className="font-medium text-white">{totalSell.toFixed(2).toString()}</label>
+      <div className="flex flex-row justify-end space-x-5">
+        <div className="flex h-10 flex-row items-center space-x-2.5 rounded-lg bg-white p-2">
+          <Image src={BoughtIcon} alt="" width={30} height={30}></Image>
+          <label className="font-medium text-blue-950">{totalBuy.toFixed(2).toString()}</label>
         </div>
-        <div className="flex flex-row items-center space-x-2.5">
-          <label className="font-medium text-white">ยอดรวม :</label>
-          <label className="font-medium text-white">{(totalSell - totalBuy).toFixed(2).toString()}</label>
+        <div className="h-auto border-l-2 border-blue-300"></div>
+        <div className="flex h-10 flex-row items-center space-x-2.5 rounded-lg bg-white p-2">
+          <Image src={SoldIcon} alt="" width={30} height={30}></Image>
+          <label className="font-medium text-blue-950">{totalSell.toFixed(2).toString()}</label>
         </div>
       </div>
     </div>
