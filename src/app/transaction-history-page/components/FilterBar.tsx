1a8618dc4ded6a54676a31b9f670ec31a0e34f0d
markthitrin <thitrinwork@gmail.com>
2025-03-22 16:34:04 +0700

    fix : transaction front-end filer

--- src/app/transaction-history-page/components/FilterBar.tsx
+++ src/app/transaction-history-page/components/FilterBar.tsx
@@ -7,49 +7,57 @@ const FilterBar = () => {
   const { filter, totalBuy, totalSell } = useTransactionContext();
 
   return (
-    <div className="flex flex-col items-center justify-between p-2.5 lg:flex-row">
+    <div className="flex flex-col items-center justify-between bg-blue-400 p-2.5 shadow-xl lg:flex-row">
       <div className="flex flex-row justify-start space-x-5">
         <div className="flex flex-row items-center space-x-2.5">
-          <label className="font-medium text-gray-800">ตั้งแต่วันที่</label>
+          <label className="font-medium text-white">ตั้งแต่วันที่</label>
           <input
             className="transform rounded-lg border border-gray-300 p-1 transition-transform duration-200 hover:scale-105 hover:shadow-xl"
             type="date"
             onChange={(e) => filter.setStartDate(e.target.value ? new Date(e.target.value) : beginningOfTime)}
           />
-          <label className="font-medium text-gray-800">ถึงวันที่</label>
+          <label className="font-medium text-white">ถึงวันที่</label>
           <input
             className="transform rounded-lg border border-gray-300 p-1 transition-transform duration-200 hover:scale-105 hover:shadow-xl"
             type="date"
             onChange={(e) => filter.setEndDate(e.target.value ? new Date(e.target.value) : endOfTime)}
           />
         </div>
+        <div className="h-auto border-l-2 border-blue-300"></div>
         <div className="flex flex-row items-center space-x-2.5">
-          <input
-            className="h-5 w-5 transition-transform duration-200"
-            type="checkbox"
-            checked={filter.asBuyer}
-            onChange={(e) => filter.setAsBuyer(e.target.checked)}
-          />
-          <label className="font-medium text-gray-800">ซื้อ</label>
-          <input
-            className="h-5 w-5 transition-transform duration-200"
-            type="checkbox"
-            checked={filter.asSeller}
-            onChange={(e) => filter.setAsSeller(e.target.checked)}
-          />
-          <label className="font-medium text-gray-800">ขาย</label>
+          <button
+            className={`h-10 w-20 rounded-lg ${filter.asBuyer ? "bg-indigo-500 text-white" : "bg-blue-200 text-gray-600"} transition-transform duration-200 hover:scale-105 hover:shadow-lg`}
+            onClick={() => filter.setAsBuyer(!filter.asBuyer)}
+          >
+            ซื้อ
+          </button>
+          <button
+            className={`h-10 w-20 rounded-lg ${filter.asSeller ? "bg-indigo-500 text-white" : "bg-blue-200 text-gray-600"} transition-transform duration-200 hover:scale-105 hover:shadow-lg`}
+            onClick={() => filter.setAsSeller(!filter.asSeller)}
+          >
+            ขาย
+          </button>
+        </div>
+        <div className="h-auto border-l-2 border-blue-300"></div>
+        <div className="flex flex-row items-center space-x-2.5">
+          <button
+            className={`h-10 w-20 rounded-lg ${filter.asSeller ? "bg-indigo-500 text-white" : "bg-blue-200 text-gray-600"} transition-transform duration-200 hover:scale-105 hover:shadow-lg`}
+            onClick={() => filter.setAsSeller(!filter.asSeller)}
+          >
+            ขาย
+          </button>
         </div>
       </div>
       <div className="flex flex-row items-center justify-start space-x-10">
         <div className="flex flex-row items-center space-x-2.5">
-          <label className="font-medium text-gray-800">ยอดซื้อ :</label>
-          <label className="font-medium text-gray-800">{totalBuy.toFixed(2).toString()}</label>
-          <label className="font-medium text-gray-800">ยอดขาย :</label>
-          <label className="font-medium text-gray-800">{totalSell.toFixed(2).toString()}</label>
+          <label className="font-medium text-white">ยอดซื้อ :</label>
+          <label className="font-medium text-white">{totalBuy.toFixed(2).toString()}</label>
+          <label className="font-medium text-white">ยอดขาย :</label>
+          <label className="font-medium text-white">{totalSell.toFixed(2).toString()}</label>
         </div>
         <div className="flex flex-row items-center space-x-2.5">
-          <label className="font-medium text-gray-800">ยอดรวม :</label>
-          <label className="font-medium text-gray-800">{(totalSell - totalBuy).toFixed(2).toString()}</label>
+          <label className="font-medium text-white">ยอดรวม :</label>
+          <label className="font-medium text-white">{(totalSell - totalBuy).toFixed(2).toString()}</label>
         </div>
       </div>
     </div>
