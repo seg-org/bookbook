3d8478098869ef8f177481a9beb4796ee6183240
Idhibhat Pankam <marginpankam@gmail.com>
2025-04-04 09:35:03 +0700

    remove eslint-plugin-simple-import-sort

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,5 @@
 import { FlatCompat } from "@eslint/eslintrc";
 import eslintPluginReact from "eslint-plugin-react";
-import simpleImportSort from "eslint-plugin-simple-import-sort";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
@@ -27,37 +26,6 @@ const eslintConfig = [
     },
     rules: {
       "react/no-unescaped-entities": "warn",
-    },
-  },
-  {
-    plugins: {
-      "simple-import-sort": simpleImportSort,
-    },
-    rules: {
-      "simple-import-sort/exports": "error",
-      "simple-import-sort/imports": [
-        "error",
-        {
-          groups: [
-            // Side effect imports.
-            ["^\\u0000"],
-            // Node.js builtins prefixed with `node:`.
-            ["^node:"],
-            // Packages.
-            // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
-            ["^@?\\w"],
-            // Absolute imports and other imports such as Vue-style `@/foo`.
-            // Anything not matched in another group.
-            ["^"],
-            // Relative imports.
-            // Anything that starts with a dot.
-            ["^\\."],
-          ],
-        },
-      ],
-      "import/first": "error",
-      "import/newline-after-import": "error",
-      "import/no-duplicates": "error",
     },
   },
 ];
