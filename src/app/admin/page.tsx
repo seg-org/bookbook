94edf3679bff712ca4ee4132310b85674da778b5
Nutthapat Pongtanyavichai <contact@leomotors.me>
2025-04-08 18:56:41 +0700

    add comment

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,6 +3,8 @@ import { verifyAdmin } from "@/lib/authorization";
 export default async function AdminPage() {
   await verifyAdmin();
 
+  // todo redirect to admin dashboard (when it's done) and remove this page
+
   return (
     <section className="p-6">
       <h1 className="mb-2 text-2xl font-bold text-red-500">hello world</h1>
