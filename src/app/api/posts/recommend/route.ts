9925f061f96a1b9c21cdb39405cdd9010d31fab8
demonstem <chanatippattanapen@gmail.com>
2025-04-17 23:51:27 +0700

    fix lint

--- src/app/api/posts/recommend/route.ts
+++ src/app/api/posts/recommend/route.ts
@@ -1,6 +1,7 @@
-import { NextRequest, NextResponse } from "next/server";
-import { prisma } from "@/lib/prisma";
 import { TransactionStatus } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+import { prisma } from "@/lib/prisma";
 
 export async function GET(req: NextRequest) {
   try {
