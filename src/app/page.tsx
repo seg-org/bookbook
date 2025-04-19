9c21733520d58eca9edd5f37b21d0a94213487be
Theepob <121664383+Bee0Theepob@users.noreply.github.com>
2025-04-19 13:48:49 +0700

    lint:fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,7 @@ import {
 } from "lucide-react";
 import Link from "next/link";
 import { getServerSession } from "next-auth";
+
 import { authOptions } from "@/lib/auth";
 
 export default async function Home() {
