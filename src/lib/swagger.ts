b4bd5681344937dea580850821f51a6bbb89370e
Chayanin Kongsareekul <57552210+Aphrodicez@users.noreply.github.com>
2025-04-22 23:38:08 +0700

    lint

--- src/lib/swagger.ts
+++ src/lib/swagger.ts
@@ -1,13 +1,13 @@
 import { OpenApiGeneratorV3 } from "@asteasolutions/zod-to-openapi";
 import { OpenAPIObjectConfig } from "@asteasolutions/zod-to-openapi/dist/v3.0/openapi-generator";
 
+import { adminRegistry } from "@/app/api/admin/swagger";
 import { bookmarkRegistry } from "@/app/api/bookmarks/swagger";
 import { bookRegistry } from "@/app/api/books/swagger";
 import { chatRegistry } from "@/app/api/chat/swagger";
 import { objectRegistry } from "@/app/api/objects/swagger";
 import { postRegistry } from "@/app/api/posts/swagger";
 import { transactionRegistry } from "@/app/api/transaction/swagger";
-import { adminRegistry } from "@/app/api/admin/swagger";
 
 export const getApiDocs = async () => {
   const generator = new OpenApiGeneratorV3([
